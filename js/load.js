be4fde1f35bd382b05df2db476eef2e16af8e8f5
k-takahashi <keita.takahashi.78@gmail.com>
2020-05-19 11:35:49 +0900

    11:35

---
+++ js/load.js
@@ -0,0 +1,9 @@
+$(function() {
+ $('.header-container').load('parts/header.html');
+});
+$(function() {
+ $('.nav-container').load('parts/nav.html');
+});
+$(function() {
+ $('.footer-container').load('parts/footer.html');
+});
--- load.js
+++
@@ -1,9 +0,0 @@
-$(function() {
- $('.header-container').load('header.html');
-});
-$(function() {
- $('.nav-container').load('nav.html');
-});
-$(function() {
- $('.footer-container').load('footer.html');
-});
